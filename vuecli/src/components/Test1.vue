77444f28dd971eca501d3a30840907dc4a088436
melodymateng <370627276@qq.com>
2018-02-01 22:36:55 +0800

    VUECLI

---
+++ vuecli/src/components/Test1.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="test1">
+        <h2>我是测试页面1</h2>
+        <router-link to="/subTest1/">subTest1</router-link>
+        <router-link to="/subTest2/">subTest2</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .test1{
+    height: 300px;
+    background: #ff0000;
+  }
+</style>
---
+++ vuecli/src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Test1 from '@/components/Test1'
+import subTest1 from '@/components/subTest1'
+import subTest2 from '@/components/subTest2'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/test/',
+      name: 'Test1',
+      component: Test1,
+      children:[
+        {
+          path: '/subTest1/',
+          component:subTest1
+        },
+        {
+          path: '/subTest2/',
+          component:subTest2
+        }
+      ]
+    }
+  ]
+})
